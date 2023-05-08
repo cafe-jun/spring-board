7eb52338533ac847476dd031ef5b04b2dcf98b48
jsshin <tb25271@gmail.com>
2023-05-08 19:22:46 +0900

    [feat] spring 초기 세팅

---
+++ src/test/java/com/cafejun/springboard/SpringBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cafejun.springboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
