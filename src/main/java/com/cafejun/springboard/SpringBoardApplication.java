287276574cc9137941edba8ec296cca387627976
jsshin <tb25271@gmail.com>
2023-10-25 21:53:51 +0900

    CloudType으로 배포 세팅
    CloudType으로 배포 세팅

--- src/main/java/com/cafejun/springboard/SpringBoardApplication.java
+++ src/main/java/com/cafejun/springboard/SpringBoardApplication.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.ComponentScan;
 @ConfigurationPropertiesScan // 이게 없으면 custom propert가 스캔이 안됨
 @SpringBootApplication
 public class SpringBoardApplication {
-
+	// CloudType으로 배포 세팅
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBoardApplication.class, args);
 	}
